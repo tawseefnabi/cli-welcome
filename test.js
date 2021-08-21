d350ec87964ff943ebacee845fbd064383f3a46c
tawseef <tawseefnabi9@gmail.com>
2021-08-21 22:35:49 +0530

    npm publish test

---
+++ test.js
@@ -0,0 +1,15 @@
+const welcome = require('./index.js');
+
+// Use it.
+welcome({
+	title: `Welcome CLI`,
+	tagLine: `by Tawseef`,
+	description: `Create a welcome message for your CLI.`,
+	bgColor: `#FADC00`,
+	color: `#000000`,
+	bold: true,
+	clear: true,
+	version: `v1.0`
+});
+
+console.log(`\n\n\n\n\n`);
